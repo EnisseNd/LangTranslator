bea5f04ad366d836711317d8de57f1ccf10abcf4
enissend <enissendikumana07@gmail.com>
2025-05-18 16:01:07 -0500

    Set up main window for Language Translator application

--- solution.py
+++ solution.py
@@ -2,5 +2,8 @@ from googletrans import LANGUAGES
 from tkinter import *
 from tkinter import ttk
 
-
-print(LANGUAGES)
+# Create the main window (using root)
+root = Tk()
+root.geometry('1200x600')
+root['bg'] = 'purple'
+root.title('Language Translator')
